99c94452fe2884ca71c3628c1d00aa5450cc38a5
Oskar Asplin <oskarba@stud.ntnu.no>
2016-01-14 14:30:21 +0100

    first commit

---
+++ exercise1.py
@@ -0,0 +1,31 @@
+from threading import Thread
+
+i = 0
+
+class MyThread(Thread):
+	def __init__(self, val):
+		''' Constructor. '''
+		Thread.__init__(self)
+		self.val = val
+	def run(self):
+		global i
+		if self.val == 0:
+			for j in range(0, 1000000):
+				i +=1
+		if self.val == 1:
+			for j in range(0, 1000000):
+				i -=1
+
+def main():
+	myThreadOb1 = MyThread(0)
+	myThreadOb1.setName('Thread 1')
+	myThreadOb2 = MyThread(1)
+	myThreadOb2.setName('Thread 2')
+	# Start running the threads!
+	myThreadOb1.start()
+	myThreadOb2.start()
+	myThreadOb1.join()
+	myThreadOb2.join()
+	print i
+
+main()
