c012d755ec7df3fe74013a91197ae9e2cb05753d
borgewi <borgewi@stud.ntnu.no>
2016-01-15 17:58:19 +0100

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,35 @@
+from threading import Thread, Lock
+#hshshshsh
+i = 0
+lock = Lock()
+class MyThread(Thread):
+	def __init__(self, val):
+		''' Constructor. '''
+		Thread.__init__(self)
+		self.val = val
+	def run(self):
+		global i
+		if self.val == 0:
+			for j in range(0, 1000000):
+				lock.acquire() 
+				i +=1
+				lock.release()
+		if self.val == 1:
+			for j in range(0, 1000000):
+				lock.acquire()
+				i -=1
+				lock.release()
+
+def main():
+	myThreadOb1 = MyThread(0)
+	myThreadOb1.setName('Thread 1')
+	myThreadOb2 = MyThread(1)
+	myThreadOb2.setName('Thread 2')
+	# Start running the threads!
+	myThreadOb1.start()
+	myThreadOb2.start()
+	myThreadOb1.join()
+	myThreadOb2.join()
+	print i
+
+main()
