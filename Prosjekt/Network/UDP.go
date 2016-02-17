b25fb28b87a7021e68dea7933450e7d311279723
oskarba <oskarba@stud.ntnu.no>
2016-02-17 14:58:48 +0100

    Update UDP.go

--- Prosjekt/Network/UDP.go
+++ Prosjekt/Network/UDP.go
@@ -1,82 +1,133 @@
-package main
- 
+package udp
+
 import (
-    "fmt"
-    "net"
-    "os"
-    "strings"
-    "strconv"
-    "encoding/binary"
-    "time"
+	"fmt"
+	"net"
+	"strconv"
 )
 
+var laddr *net.UDPAddr //Local address
+var baddr *net.UDPAddr //Broadcast address
 
-func CheckError( err error ) {
-    if err  != nil {
-        fmt.Println( "Error: " , err )
-        os.Exit(0)
-    }
+type Udp_message struct {
+	Raddr  string //if receiving raddr=senders address, if sending raddr should be set to "broadcast" or an ip:port
+	Data   string //TODO: implement another encoding, strings are meh
+	Length int    //length of received data, in #bytes // N/A for sending
 }
 
-//New udp initialize function, which uses resolveUDPAddr, DialUDP, and ListenUDP
+func Udp_init(localListenPort, broadcastListenPort, message_size int, send_ch, receive_ch chan Udp_message) (err error) {
+	//Generating broadcast address
+	baddr, err = net.ResolveUDPAddr("udp4", "255.255.255.255:"+strconv.Itoa(broadcastListenPort))
+	if err != nil {
+		return err
+	}
 
-func udpBroadcast( toAdress string , activationTime int64 ) {
-    ServerAddress,err := net.ResolveUDPAddr( "udp" , toAdress  )
-    CheckError( err )
+	//Generating localaddress
+	tempConn, err := net.DialUDP("udp4", nil, baddr)
+	defer tempConn.Close()
+	tempAddr := tempConn.LocalAddr()
+	laddr, err = net.ResolveUDPAddr("udp4", tempAddr.String())
+	laddr.Port = localListenPort
 
-    Connection, err := net.DialUDP( "udp", nil, ServerAddress ) //Returns type UDPConn which we can send msg to
-    CheckError( err )
+	//Creating local listening connections
+	localListenConn, err := net.ListenUDP("udp4", laddr)
+	if err != nil {
+		return err
+	}
 
-    defer Connection.Close() //Closes the connection after udpBroadcast functioncall
+	//Creating listener on broadcast connection
+	broadcastListenConn, err := net.ListenUDP("udp", baddr)
+	if err != nil {
+		localListenConn.Close()
+		return err
+	}
 
-    for {
+	go udp_receive_server(localListenConn, broadcastListenConn, message_size, receive_ch)
+	go udp_transmit_server(localListenConn, broadcastListenConn, send_ch)
 
-        buffer := make( []byte, 1024 )
-        binary.PutVarint( buffer , activationTime )
-
-        _,err := Connection.Write( buffer )
-        CheckError( err )
-
-        time.Sleep( time.Millisecond * 1000 ) //Will we be needing much sleeptime?
-    }
+	//	fmt.Printf("Generating local address: \t Network(): %s \t String(): %s \n", laddr.Network(), laddr.String())
+	//	fmt.Printf("Generating broadcast address: \t Network(): %s \t String(): %s \n", baddr.Network(), baddr.String())
+	return err
 }
 
-func listenToBroadcast( fromPort string ) {
+func udp_transmit_server(lconn, bconn *net.UDPConn, send_ch chan Udp_message) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("ERROR in udp_transmit_server: %s \n Closing connection.", r)
+			lconn.Close()
+			bconn.Close()
+		}
+	}()
 
-    ServerAddress,err := net.ResolveUDPAddr( "udp" , ":" + fromPort)
-    CheckError(err)
+	var err error
+	var n int
 
-    ServerConnection, err := net.ListenUDP( "udp" , ServerAddress )
-    CheckError(err)
-    
-    defer ServerConnection.Close()
-    
-    buffer := make( []byte, 1024 )
- 
-    for {
-        n, senderAddress ,err := ServerConnection.ReadFromUDP( buffer ) //Might want senderAddr
-        CheckError( err )
-
-        receivedActivationTime, _ := binary.Varint(buffer[0:n])
-        fmt.Println( "Received IP adress" ,  senderAddress.IP.String() , "and activation time" ,receivedActivationTime)
-        //fmt.Println(serverConn.receiveFrom()) 
-    }
-    //receivedId := elevatorId{ activationTime: strconv.Atoi(receivedStrings[0]) , elevatorIndex: strconv.Atoi(receivedStrings[1]) }
-    //return receivedId
+	for {
+		//		fmt.Printf("udp_transmit_server: waiting on new value on Global_Send_ch \n")
+		msg := <-send_ch
+		//		fmt.Printf("Writing %s \n", msg.Data)
+		if msg.Raddr == "broadcast" {
+			n, err = lconn.WriteToUDP([]byte(msg.Data), baddr)
+		} else {
+			raddr, err := net.ResolveUDPAddr("udp", msg.Raddr)
+			if err != nil {
+				fmt.Printf("Error: udp_transmit_server: could not resolve raddr\n")
+				panic(err)
+			}
+			n, err = lconn.WriteToUDP([]byte(msg.Data), raddr)
+		}
+		if err != nil || n < 0 {
+			fmt.Printf("Error: udp_transmit_server: writing\n")
+			panic(err)
+		}
+		//		fmt.Printf("udp_transmit_server: Sent %s to %s \n", msg.Data, msg.Raddr)
+	}
 }
 
-func main() {
-    //****************************
-    referenceTime := time.Unix(1451606400,0) //Unix 2016 01/01/00
-    currentTime := time.Now() //returns local unix time
+func udp_receive_server(lconn, bconn *net.UDPConn, message_size int, receive_ch chan Udp_message) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("ERROR in udp_receive_server: %s \n Closing connection.", r)
+			lconn.Close()
+			bconn.Close()
+		}
+	}()
 
-    var duration time.Duration = currentTime.Sub( referenceTime )
-    var activationTime = int64(duration)
-    //****************************
+	bconn_rcv_ch := make(chan Udp_message)
+	lconn_rcv_ch := make(chan Udp_message)
 
-    //udpBroadcast( "129.241.187.255:30007" , activationTime )
+	go udp_connection_reader(lconn, message_size, lconn_rcv_ch)
+	go udp_connection_reader(bconn, message_size, bconn_rcv_ch)
+
+	for {
+		select {
+
+		case buf := <-bconn_rcv_ch:
+			receive_ch <- buf
+
+		case buf := <-lconn_rcv_ch:
+			receive_ch <- buf
+		}
+	}
 }
 
-//ch <- v    // Send v to channel ch.
-//v := <-ch  // Receive from ch, and
-//           // assign value to v.
+func udp_connection_reader(conn *net.UDPConn, message_size int, rcv_ch chan Udp_message) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("ERROR in udp_connection_reader: %s \n Closing connection.", r)
+			conn.Close()
+		}
+	}()
+
+	for {
+		buf := make([]byte, message_size)
+		//		fmt.Printf("udp_connection_reader: Waiting on data from UDPConn\n")
+		n, raddr, err := conn.ReadFromUDP(buf)
+		//		fmt.Printf("udp_connection_reader: Received %s from %s \n", string(buf), raddr.String())
+		if err != nil || n < 0 {
+			fmt.Printf("Error: udp_connection_reader: reading\n")
+			panic(err)
+		}
+		rcv_ch <- Udp_message{Raddr: raddr.String(), Data: string(buf), Length: n}
+	}
+}
